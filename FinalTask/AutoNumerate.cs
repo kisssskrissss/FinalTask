620e619f0b5419989cf7ab242d8d626edacab86f
krist <krist@LAPTOP-HIQLHLL6>
2023-02-25 11:56:29 +0500

    Добавьте файлы проекта.

---
+++ FinalTask/AutoNumerate.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FinalTask
+{
+    [Transaction(TransactionMode.Manual)]
+    public class AutoNumerate : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                var doc = commandData.Application.ActiveUIDocument.Document;
+                List<Room> rooms = new FilteredElementCollector(doc)
+                    .OfCategory(BuiltInCategory.OST_Rooms)
+                    .OfType<Room>()
+                    .ToList();
+
+                int number= 1;
+                Transaction transaction = new Transaction(doc, "Автоматическая нумерация помещений");
+                transaction.Start();
+                foreach (Room room in rooms)
+                {
+                    Parameter param = room.get_Parameter(BuiltInParameter.ROOM_NUMBER);
+                    param.Set(number.ToString());
+                    number++;
+                }
+                transaction.Commit();
+            }
+            catch
+            {
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
